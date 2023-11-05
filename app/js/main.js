ca0b1eca192deb1cbc8e4ad651044c9d14bff4cb
s0litude0ne <solitudeone@mail.ru>
2023-11-06 01:24:27 +0500

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -22,16 +22,24 @@ $(function () {
 		prefix: "$"
 	});
 
-	$('.icon-th-list').on('click', function(){
+	$('.icon-th-list').on('click', function () {
 		$('.product__item').addClass('list');
 		$('.icon-th-list').addClass('active');
 		$('.icon-th-large').removeClass('active');
 	});
-	$('.icon-th-large').on('click', function(){
+	$('.icon-th-large').on('click', function () {
 		$('.product__item').removeClass('list');
 		$('.icon-th-large').addClass('active');
 		$('.icon-th-list').removeClass('active');
 	});
+
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle();
+	})
+
+	$('.header__btn-menu').on('click', function () {
+		$('.header__box').toggleClass('active');
+	})
 
 
 	var mixer = mixitup('.products__inner-box');
